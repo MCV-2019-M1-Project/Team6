614c937a24548d3d33a10d7028b86f28b9413142
Albert <albert.jimenez.tauste@gmail.com>
2019-10-20 16:26:08 +0200

    2 images case (compilation errors)

--- week2/main.py
+++ week2/main.py
@@ -28,7 +28,7 @@ DIST_METRIC= cfg['dist'] #'euclidean' 'chisq' or 'hellinger'
 BG_REMOVAL = cfg['bgrm'] # 1, 2 or 3 bg removal method
 QUERY_SET= cfg['queryset'] # Input query set
 
-K = 3
+K = 1
 
 ## FUNCTIONS ##
 def text_removal_mask(img_gray, name, strel, strel_pd, num_cols, coords, background_mask):
@@ -36,84 +36,92 @@ def text_removal_mask(img_gray, name, strel, strel_pd, num_cols, coords, backgro
     # Obtain image dimensions
     height,width = img_gray.shape[:2]
     
-    # Create variable where final mask will be stored
-    f_mask = np.ones(shape=(height,width))
-    f_mask.fill(255)
+    c=[]
     
-    # Boundaries of the analyzed area
-    min_a = round(width/2)
-    max_a = round(min_a + num_cols)
-    
-    # Store pixel values in the analyzed area
-    values_t = np.zeros(shape=(max_a-min_a, max_a-min_a))
-    
-    i = 0
-    
-    for p in range(min_a, max_a):
-        # Per each column, compute number of ocurrences of every pixel value
-        col = img_gray[:,p]
+    for picture in range(len(background_mask)):
+        # Create variable where final mask will be stored
+        f_mask = np.ones(shape=(height,width))
+        f_mask.fill(255)
         
-        # Pixel values and number of ocurrences for the whole column
-        values = pd.Series(col).value_counts().keys().tolist()
+        # Boundaries of the analyzed area
+        columns = background_mask.sum(axis=0) 
+        min_a = round((np.argmax(columns!=0)+np.argmin(columns!=0))/2)
+        max_a = round(min_a + num_cols)
+
+        # Store pixel values in the analyzed area
+        values_t = np.zeros(shape=(max_a-min_a, max_a-min_a))
         
-        # Get highest pixel values (most frequent ones)
-        values_t[0:4,i] = values[0:4]
-
-        i += 1
-
-    j = 0
-    w = 0
-    h = 0
-
-    while((w*h < 20000 or w*h > 1000000 or w<h) and j < num_cols):
-
-        level = round(np.mean(values_t[j,:]))
+        i = 0
         
-        if level <= 128:
-            final_img = cv.morphologyEx(img_gray, cv.MORPH_OPEN, strel)
-            #mask = final_img == level #Method 1
-            mask = (final_img >= max(0,level-1)) * (final_img <= level+1) #Method 2
-            mask = mask.astype(np.uint8)
-            mask *= 255
-        elif level > 128:
-            final_img = cv.morphologyEx(img_gray, cv.MORPH_CLOSE, strel)
-            #mask = final_img == level #Method 1
-            mask = (final_img >= max(0,level-1)) * (final_img <= level+1) #Method 2
-            mask = mask.astype(np.uint8)
-            mask *= 255
-
-        if np.sum(mask) != 0:
-
-            # Find contours of created mask
-            contours,_ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-
-            # Find largest contour (it will contain the text bounding box)
-            contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
-            largest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+        for p in range(min_a, max_a):
+            # Per each column, compute number of ocurrences of every pixel value
+            col = img_gray[:,p]
             
-            # Find bounding box belonging to detected contour
-            (x,y,w,h) = cv.boundingRect(largest_contour)
-
-            # Draw bounding boxes coordinates on original image to visualize it
-            cv.rectangle(img_gray, (x,y), (x+w,y+h), (0,255,0), 2)
+            # Pixel values and number of ocurrences for the whole column
+            values = pd.Series(col).value_counts().keys().tolist()
             
-            # Bboxes coordinates of the text positions
-            tlx = x
-            tly = y
-            brx = x + w
-            bry = y + h
+            # Get highest pixel values (most frequent ones)
+            values_t[0:4,i] = values[0:4]
 
-            # Extract bounding boxes pixels 
-            bb_size = w*h
+            i += 1
 
-        j+=1
+        j = 0
+        w = 0
+        h = 0
 
-    # Add bboxes coordinates to a list of lists
-    coords.append([(tlx,tly,brx,bry)])
+        while((w*h < 20000 or w*h > 1000000 or w<h) and j < num_cols):
 
-    # Create new mask with bboxes coordinates
-    # Bboxes pixels are black (text), white otherwise (painting)
-    f_mask[y:y + h, x:x + w] = 0
+            level = round(np.mean(values_t[j,:]))
+            
+            if level <= 128:
+                final_img = cv.morphologyEx(img_gray, cv.MORPH_OPEN, strel)
+                #mask = final_img == level #Method 1
+                mask = (final_img >= max(0,level-1)) * (final_img <= level+1) #Method 2
+                mask = mask.astype(np.uint8)
+                mask = mask*background_mask[picture]
+                #mask *= 255
+            elif level > 128:
+                final_img = cv.morphologyEx(img_gray, cv.MORPH_CLOSE, strel)
+                #mask = final_img == level #Method 1
+                mask = (final_img >= max(0,level-1)) * (final_img <= level+1) #Method 2
+                mask = mask.astype(np.uint8)
+                mask = mask*background_mask[picture]
+                #mask *= 255
+
+            if np.sum(mask) != 0:
+
+                # Find contours of created mask
+                contours,_ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+                # Find largest contour (it will contain the text bounding box)
+                contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
+                largest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+                
+                # Find bounding box belonging to detected contour
+                (x,y,w,h) = cv.boundingRect(largest_contour)
+
+                # Draw bounding boxes coordinates on original image to visualize it
+                cv.rectangle(img_gray, (x,y), (x+w,y+h), (0,255,0), 2)
+                
+                # Bboxes coordinates of the text positions
+                tlx = x
+                tly = y
+                brx = x + w
+                bry = y + h
+            j+=1
+
+        # Add bboxes coordinates to a list of lists
+        if QUERY_SET != 'qsd2_w2' and QUERY_SET != 'qst2_w2':
+            coords.append([(tlx,tly,brx,bry)])
+        elif picture < len(background_mask)-1:
+            c.append([(tlx,tly,brx,bry)])
+        else:
+            c.append([(tlx,tly,brx,bry)])
+            coords.append([c])
+
+        # Create new mask with bboxes coordinates
+        # Bboxes pixels are black (text), white otherwise (painting)
+        f_mask[y:y + h, x:x + w] = 0
 
     cv.imwrite('results/'+ name + 'txtrmask.png', f_mask)
     
@@ -259,7 +267,7 @@ def compute_mask(img,name):
         mask_left = mask_left*mask
         mask_right = mask_right*mask
         mask = [mask_left, mask_right]        
-    
+
     return mask, eval_metrics
 
 def extract_features(img,mask):
