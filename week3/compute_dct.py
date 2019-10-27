00a8accd889b518b439a833b2d1d1aa93e51a28f
jordiburgues <jordiburguesm@gmail.com>
2019-10-27 14:30:40 +0200

    changes in DCT

--- week3/compute_dct.py
+++ week3/compute_dct.py
@@ -6,73 +6,53 @@ import numpy as np
 from numpy import r_
 import matplotlib.pyplot as plt
 
+# descriptor = compute_dct(img_gray, 8, 16)
 
+def compute_dct(im, block_size, num_coeffs):
+    # List to store kept DCT coefficients
+    dct_coeffs_list = []
+    # Resize image to speed up execution
+    im = cv.resize(im, (512, 512)) 
+    h, w = im.shape[:2]
+    # Variable to store DCT
+    dct_im = np.zeros([h,w], dtype=np.uint8)
+
+    #print("Num of blocks" + str((256*256)/(block_size*block_size)))
+
+    for i in range(0, h, block_size):
+        for j in range(0, w, block_size):
+            # Compute DCT of current block
+            block = im[i:(i+block_size),j:(j+block_size)]
+            block_float = np.float32(block)
+            block_dct = cv.dct(block_float)
+            dct_im[i:(i+block_size),j:(j+block_size)] = np.uint8(block_dct)*255.0
+            dct_block = dct_im[i:(i+block_size),j:(j+block_size)]
+            #Zig-zag scan of DCT block
+            dct_coeffs = np.concatenate([np.diagonal(dct_block[::-1,:], i)[::(2*(i % 2)-1)] for i in range(1-dct_block.shape[0], dct_block.shape[0])])
+            # Keep N coefficients
+            dct_coeffs = dct_coeffs[0:num_coeffs]
+            dct_coeffs_list.append(dct_coeffs)
+
+    dct_coeffs_list = np.hstack(dct_coeffs_list)
+
+    final_list = []
+    for item in dct_coeffs_list:
+        final_list.append([item])
+
+    return final_list, dct_im
+
+
+"""
 im = cv.imread('../qs/qsd1_w2/00003.jpg', cv.IMREAD_COLOR)
 img_gray =  cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 
-dct_coeffs_list = []
-# Extract image dimensions
-h, w = img_gray.shape[:2]
-
-# DCT block size
-block_size = 8
-
-# Number of DCT coefficients to keep
-num_coeffs = 8
-
-# Resize image in case it is odd
-if (h % 2 == 1):
-    h += 1
-
-if (w % 2 == 1):
-    w += 1
-
-resized_img = cv.resize(img_gray, (w, h)) 
-
-# Remove salt and pepper noise
-# filtered_im = cv.medianBlur(resized_img, 3)
-
-filtered_im = resized_img
-
-cv.imshow("image", filtered_im)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-# Normalize filtered image
-im_float = np.float32(filtered_im)/255.0
-# Compute dct coefficients
-im_dct = cv.dct(im_float)
-# Convert to original format
-img = np.uint8(im_dct)*255.0
-# Variable to store DCT
-dct_im = np.zeros([h,w], dtype=np.uint8)
-
-print("Num of blocks" + str((h*w)/block_size))
-
-for i in r_[:h:block_size]:
-    for j in r_[:w:block_size]:
-        # Compute DCT of current block
-        block_float = np.float32(filtered_im[i:(i+8),j:(j+8)])
-        block_dct = cv.dct(block_float)
-        dct_im[i:(i+8),j:(j+8)] = np.uint8(block_dct)*255.0
-        dct_block = dct_im[i:(i+8),j:(j+8)]
-        #Zig-zag scan of DCT block
-        dct_coeffs = np.concatenate([np.diagonal(dct_block[::-1,:], i)[::(2*(i % 2)-1)] for i in range(1-dct_block.shape[0], dct_block.shape[0])])
-        print(len(dct_coeffs))
-        # Keep N coefficients
-        dct_coeffs = dct_coeffs[0:num_coeffs]
-        dct_coeffs_list.append(dct_coeffs)
-
-cv.imshow("image", dct_im)
-cv.waitKey(0)
-cv.destroyAllWindows()
-
+dct_coeffs_list, dct_im = compute_dct(img_gray, 8, 16)
 #print(dct_coeffs_list)
-"""
-# Display entire DCT
-plt.imshow(dct_im,cmap='gray',vmax = np.max(dct_im)*0.01,vmin = 0)
-plt.title( "8x8 DCTs of the image")
-plt.show()
+#print(len(dct_coeffs_list))
 
-print(img)
+print(dct_coeffs_list)
+
+cv.imshow("dct_image", dct_im)
+cv.waitKey(0)
+cv.destroyAllWindows()
 """
