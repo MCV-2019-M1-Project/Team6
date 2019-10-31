62ddd3c97bad9f8cd2c0501057a293b219cb45e2
Eloi Bové <eloibove@gmail.com>
2019-10-31 12:27:20 +0100

    W3 cleanup and W$ setup

---
+++ week4/main.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python3.5
+## PYTHON LIBS ##
+import cv2 as cv
+import numpy as np
+import glob
+import pickle
+import ml_metrics
+import math
+import pandas as pd
+import os
+import yaml
+from matplotlib import pyplot as plt 
+from evaluation_funcs import performance_accumulation_pixel
+from evaluation_funcs import performance_evaluation_pixel
+from bbox_iou import bbox_iou
+
+
+## CUSTOM LIBS ## 
+from extract_features import extract_features
+from compute_mask import compute_mask
+from compute_mask_old import compute_mask_old
+from text_removal_mask import text_removal_mask
+from text_removal_mask2 import find_text
+from search_queries import search
+from compute_lbp import compute_lbp
+from compute_hog import compute_hog
+from get_text import get_text
+
+## PARAMETERS ##
+with open("config.yml", 'r') as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+if cfg['colorspace'] == 'HSV' :
+    COLORSPACE = cv.COLOR_BGR2HSV
+elif cfg['colorspace'] == 'YUV' :
+    COLORSPACE = cv.COLOR_BGR2YUV
+elif cfg['colorspace'] == 'LAB' :
+    COLORSPACE = cv.COLOR_BGR2Lab
+elif cfg['colorspace'] == 'RGB' : 
+    COLORSPACE = cv.COLOR_BGR2RGB 
+
+NBINS = cfg['nbins']        # Number of bins (from 0 to 255)
+DIVISIONS = cfg['divs']     # Number of divisions per dimension [2,4,8,...]
+DIST_METRIC= cfg['dist']    #'euclidean' 'chisq' or 'hellinger'
+BG_REMOVAL = cfg['bgrm']    # 1, 2 or 3 bg removal method
+QUERY_SET= cfg['queryset']  # Input query set
+K = 10                      # find K closest images
+
+def main():
+
+    # Read the image database
+    database = []
+    i = 0
+    for f in sorted(glob.glob('../database/*.jpg')):
+        # Read image
+        img = cv.imread(f, cv.IMREAD_COLOR)
+
+        # Apply median blur
+        img = cv.medianBlur(img,3)
+
+        # Colorspace changes
+        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = cv.cvtColor(img, COLORSPACE)
+
+        # Compute descriptors
+        descriptor_1 = compute_lbp(img_gray, None, 8, 16, 8, 2, 'uniform')
+        descriptor_2 = extract_features(img, None, NBINS, DIVISIONS)
+        #descriptor_3 = compute_hog(img, None, 2)
+        #descriptor_4 = compute_dct(img, 8, 64)
+
+        descriptor = descriptor_1 + descriptor_2
+
+        # Store the descriptor
+        database.append(descriptor)
+
+        print(str(i))
+        i+=1
+    print('Image database read!')
+
+    # Read the text database 
+    database_txt = []
+    for f in sorted(glob.glob('../database_text/*.txt')):
+        with open(f, encoding="ISO-8859-1") as textfile:
+            line = str(textfile.readline())
+            line = line.strip("'()")
+            line = line.split(', ')
+            author = line[0].strip("'")
+            if author == '':
+                author = '#################'
+            database_txt.append( author.lower() )
+
+    print('Text database read!')
+    print('Database has ' + str(len(database)) + ' images')
+    print(database_txt)
+
+    # Evaluation metrics storing arrays
+    qs_l = '../qs/' + QUERY_SET + '/*.jpg'
+    nqueries = 30
+    precision = np.zeros(30)
+    recall = np.zeros(30)
+    fscore = np.zeros(30)
+    iou = np.zeros(30)
+
+    # Read and process the queries
+    final_ranking = []
+    coords = []
+    i = 0
+    qst_txt = []
+    for f in sorted(glob.glob(qs_l)):
+        # Read image 
+        name = os.path.splitext(os.path.split(f)[1])[0]
+        im = cv.imread(f, cv.IMREAD_COLOR)
+
+        # Remove salt and pepper noise
+        im = cv.medianBlur(im, 3)
+        
+        # Color conversions
+        img = cv.cvtColor(im, COLORSPACE)
+        img_gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+
+        bg_mask = None
+        # NO BACKGROUND
+        if QUERY_SET == 'qsd1_w2' or QUERY_SET == 'qsd1_w3' or QUERY_SET == 'qst1_w3':
+            bg_mask = None
+
+        # BACKGROUND REMOVAL
+        elif QUERY_SET == 'qsd2_w2' or QUERY_SET == 'qsd2_w3' or QUERY_SET == 'qst2_w3' or QUERY_SET == 'qsd1_w4'or QUERY_SET == 'qst1_w4':
+            bg_mask, eval_metrics = compute_mask(img,name,QUERY_SET)
+
+            if eval_metrics is not None:
+                precision[i] = eval_metrics[0]
+                recall[i] = eval_metrics[3]
+                fscore[i] = eval_metrics[4]
+        
+
+        # TEXT REMOVAL
+        # Use the mask created (image without background) to indicate search text
+        #mask, pred_coords = text_removal_mask(img_gray, name, kernel, post_kernel, num_cols, coords, bg_mask, QUERY_SET)
+        
+        if bg_mask is not None:
+            mask = find_text(img_gray, bg_mask, name)
+        else:
+            bg_mask = [np.ones((img_gray.shape[0],img_gray.shape[1]))]
+            mask = find_text(img_gray, bg_mask, name)
+        
+        #mask = bg_mask # No text removal mask
+
+        #TEXT DETECTION
+        final_authors, found_text = get_text(img_gray, mask, database_txt)
+        qst_txt.append( final_authors )
+        print(found_text[0])
+        file_auth =  open('../qs/' + QUERY_SET + '/author_' + str(i) + '.txt','w') 
+        print(len(found_text))
+        if len(found_text) == 1:
+            print(found_text[0], file = file_auth)
+        else:
+            print(found_text[1])
+            print(found_text[1], file = file_auth)
+            print(found_text[0], file = file_auth)
+        
+        file_auth.close()
+
+        # Iterate the masks (1 or 2 according to the images)
+        length = np.shape(mask)[0]
+        print(length)
+        if length > 2:
+            length = 1
+            mask = [mask]
+        
+        pre_list = []
+        for m in range(length):
+            # Use either one or the other mask
+            prod = cv.bitwise_not(mask[m]) * bg_mask[m]
+            prod = prod.astype(np.uint8)
+
+            # Extract the features
+            descriptor_1 = compute_lbp(img_gray, prod, 8, 16, 8, 2, 'uniform')
+            descriptor_2 = extract_features(img, prod, NBINS, DIVISIONS)
+            #descriptor_3 = compute_hog(img, prod, 2)
+            #descriptor_4 = compute_dct(img, 8, 64)
+             
+            print(np.shape(descriptor_1))
+            descriptor = descriptor_1 + descriptor_2
+            
+            # Search the query in the DB
+            rank = search([descriptor], database, DIST_METRIC, K)
+            print(rank)
+            pre_list.append(rank)
+
+        final_ranking.append(pre_list)
+        i += 1
+
+    # Print the final ranking
+    print('FINAL RANKING:')
+    print(len(final_ranking))
+    print(final_ranking)
+
+    print('AUTHORS:')
+    print(qst_txt)
+            
+
+    # Print the evaluation metrics
+    if QUERY_SET == 'qsd1_w2' or QUERY_SET == 'qsd2_w2' or QUERY_SET == 'qsd1_w3' or QUERY_SET == 'qsd2_w3' or QUERY_SET == 'qsd1_w4'or QUERY_SET == 'qst1_w4':
+
+        print('Query set has ' + str(nqueries) + ' images')
+        print('Precision: ' + str(np.mean(precision)))
+        print('Recall: ' + str(np.mean(recall)))
+        print('F-measure: ' + str(np.mean(fscore)))
+
+        gt = pickle.load(open('../qs/' + QUERY_SET + '/gt_corresps.pkl','rb'))
+
+        mapk_ = np.mean([ml_metrics.mapk([a],p,K) for a,p in zip(gt, final_ranking)])
+        #mapk_ = ml_metrics.mapk(gt, final_ranking, K)
+        print('MAP@K = '+ str(mapk_))
+
+        if qst_txt != []:
+            mapk_ = np.mean([ml_metrics.mapk([a],p,K) for a,p in zip(gt, qst_txt)])
+            #mapk_ = ml_metrics.mapk(gt, final_ranking, K)
+            print('MAP@K text = '+ str(mapk_))
+
+
+    ## WRITE OUTPUT FILES ##
+    pickle.dump(final_ranking, open('../qs/' + QUERY_SET + '/actual_corresps.pkl','wb'))
+
+if __name__== "__main__":
+    main()
---
+++ week4/text_removal_mask2.py
@@ -0,0 +1,131 @@
+# IMPORTS #
+import cv2 as cv
+import glob
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+def find_text(img, background_mask, name):
+
+    #Image pre-processing: the difference betwen the image opening and closing is computed
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (2*6-1, 2*6-1))
+    img_opening = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+    img_closing = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+    img_enhanced = img_closing - img_opening
+    img_lowpass	= cv.GaussianBlur(img_enhanced, (5,5), 20)
+
+    #Binarization: only the 4% brightest pixels are set to positive
+    height = img.shape[0]
+    width = img.shape[1]
+    npx =  height*width
+    hist = cv.calcHist([img_lowpass], [0], None, [256], [0, 255])/npx
+    prob=0
+    i=0
+    while(prob<0.004):
+        prob += hist[255-i][0]
+        i+=1
+    th=255-i
+    ret,img_binary = cv.threshold(img_lowpass,th,255,cv.THRESH_BINARY)
+
+    #Candidate text region extraction
+    kernel = np.ones((15,30), np.uint8)
+    img_dilated = cv.dilate(img_binary,kernel,iterations = 3)
+    kernel = np.ones((10,10), np.uint8)
+    img_eroded = cv.erode(img_dilated,kernel,iterations = 2)
+    img_opening = cv.morphologyEx(img_eroded, cv.MORPH_OPEN, np.ones((15,15), np.uint8))
+
+    num, labels = cv.connectedComponents(img_opening)
+    sorted_labels = labels.ravel()
+    sorted_labels = np.sort(sorted_labels)
+
+    labels = np.uint8(labels)
+    """
+    cv.imshow("img_binary", labels*255)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    """
+    #Discarting non desired regions
+    if np.shape(background_mask)[0]>1:
+        min_size = npx/(20*20)
+    else:
+        min_size = npx/(15*15)
+    max_aspect_ratio = 0.7
+    min_aspect_ratio = 1/15
+    min_occupancy_ratio = 0.5
+    min_compactness_ratio = 0.002
+    coords = []
+
+    for i in range(1, num+1):
+
+        region = labels == i
+        region = np.uint8(region)
+        positions =  np.where(labels == i)
+        size = len(positions[0])
+
+        if size < min_size:
+
+            labels[positions] = 0
+
+        else:
+
+            contours,_ = cv.findContours(region, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+            contour_sizes = [(cv.contourArea(contour), contour) for contour in contours]
+            largest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+            (x,y,w,h) = cv.boundingRect(largest_contour)
+            perimeter = cv.arcLength(largest_contour,True)
+            if h/w>max_aspect_ratio or h/w<min_aspect_ratio or len(positions[0])/(w*h)<min_occupancy_ratio or (w*h)/(perimeter*perimeter)<min_compactness_ratio:
+                labels[positions] = 0
+            else:
+                coords.append((x,y,w,h,size))
+    
+    """
+    cv.imshow("img_binary", labels*255)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    """
+    #Creating the expected number of masks by keeping the biggest bounding boxes
+    coords.sort(key=lambda x:x[4], reverse=True)
+    masks = []
+    for i in range(np.shape(background_mask)[0]):
+        if len(coords)>0:
+            (x,y,w,h,size) = coords.pop(0)
+            m = np.zeros((height,width),np.uint8)
+            m[y:y + h, x:x + w] = 255
+            masks.append(np.uint8(m))
+        else:
+            m = np.zeros((height,width),np.uint8)
+            masks.append(np.uint8(m))
+    
+    #Matching text mask to background mask (only if the image contains 2 or more paintings)
+    #NOT FINISHED!!!
+    #The idea is to compute each mask's center of mass (cx,cy) and calculate the distance with each bounding box center(x+w/2, y+h/2)
+    #in order to match text mask with background mask, that is, to put them in the same order in the list
+    #MAYBE WE CAN DO IT FOR MAX 2 PICTURES THIS WEEK SO THAT IT IS MUCH EASIER?
+    # if np.shape(background_mask)[0]>1:
+    #     for i in range(np.shape(background_mask)[0]):
+    #         moments = cv.moments(background_mask[i])
+    #         cx = int(moments["m10"] / moments["m00"])
+	#         cy = int(moments["m01"] / moments["m00"])
+    #         for j 
+    #         distance = math.sqrt(((cx-(x+w/2))**2)+((cy-(y+h/2))**2))
+    """
+    cv.imshow("mask", masks[0])
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    """
+    return masks
+
+# for f in sorted(glob.glob(qs_l)):
+#     name = os.path.splitext(os.path.split(f)[1])[0]
+#     im = cv.imread(f, cv.IMREAD_COLOR)
+
+"""
+im = cv.imread('../qs/' + QUERY_SET + '/00005.jpg', cv.IMREAD_COLOR)
+im = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+background_mask = [np.zeros((100,100))]
+masks = find_text(im, background_mask)
+
+cv.imshow("mask", masks[0])
+cv.waitKey(0)
+cv.destroyAllWindows()
+""" 
